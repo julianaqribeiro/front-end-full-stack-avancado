6d8f2c7c1575815c35ef4b15ae84122b39b50c3e
Juliana Quaresma <julianaqribeiro@hotmail.com>
2024-06-24 23:46:50 -0300

    Implementacao do evento onClick do componente tabBar

--- src/components/tabBar/TabBar.jsx
+++ src/components/tabBar/TabBar.jsx
@@ -1,18 +1,24 @@
 import './TabBar.css';
 import { useState } from 'react';
+import TituloSecudario from '../tituloSecundario/TituloSecundario'
 
 const TabBar = ({ itens }) => {
 
-    const [itemSelecionado, setItemSelecionado] = useState("");
+    const [itemSelecionado, setItemSelecionado] = useState([""])
 
-    return (               
-        <div className="tabBar">
+    return ( 
+        <div class="container-tabBar">
+            <div className="tabBar">
             {                 
-                itens?.map((item, idx) => 
-                    <button type="button" className='botao-tabBar' key={`item-${idx}`} click="">{item ? item?.toUpperCase() : ""}
+                itens.map((item, idx) => 
+                    <button type="button" className='botao-tabBar' key={`item-${idx}`} onClick={() => setItemSelecionado(item.toUpperCase()) }>{ item.toUpperCase() }
                     </button>)                
             }            
-        </div>                                     
+            </div>   
+            <div>
+                <TituloSecudario texto={ itemSelecionado }></TituloSecudario> 
+            </div>
+        </div>                                  
     );
 }
 
